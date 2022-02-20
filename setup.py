c9b4cab16e121010d07d84c8747115afde5b20ae
Girijesh Prasad <girijeshcse@gmail.com>
2022-02-20 09:41:53 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Capstone project for NLP ticket assignment',
+    author='ATA-G5',
+    license='MIT',
+)
